9cb8013e2a65b31f793e7b80822654ba53dddd2f
Johan Persson <johan162@gmail.com>
2022-07-10 20:25:11 +0200

    v1.4.0

--- themeltspice.rb
+++ themeltspice.rb
@@ -1,14 +1,9 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Themeltspice < Formula
   desc "Set or create a named color theme for LTSpice Circuit Simulator"
   homepage "https://github.com/johan162/ltspice-osx-themes"
-  url "https://github.com/johan162/themeltspice/archive/refs/tags/v1.3.5.tar.gz"
-  sha256 "a35fda9038e48959b76a3eb26efb2f1bc6cce8d5c84fe24fbc2b4bc4c8679436"
+  url "https://github.com/johan162/themeltspice/archive/refs/tags/v1.4.0.tar.gz"
+  sha256 "c083c2feddeed59e1b815ccd20035666d7c36f825db2e2b310fbfc1c6fdec338"
   license "MIT"
-
-  # depends_on "cmake" => :build
 
   def install
       bin.install "themeltspice.sh"
