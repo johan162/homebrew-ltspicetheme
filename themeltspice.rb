a1beb30735243fd8ffeb65dd8b959b44c56439e1
Johan Persson <johan162@gmail.com>
2022-07-14 22:34:56 +0200

    Update for v1.5.0

--- themeltspice.rb
+++ themeltspice.rb
@@ -1,8 +1,8 @@
 class Themeltspice < Formula
   desc "Set or create a named color theme for LTSpice Circuit Simulator"
   homepage "https://github.com/johan162/ltspice-osx-themes"
-  url "https://github.com/johan162/themeltspice/archive/refs/tags/v1.4.0.tar.gz"
-  sha256 "c083c2feddeed59e1b815ccd20035666d7c36f825db2e2b310fbfc1c6fdec338"
+  url "https://github.com/johan162/themeltspice/archive/refs/tags/v1.5.0.tar.gz"
+  sha256 "c158d07372c6d84795221e8c43fecf215a51b7440092c53ead20684407dd0225"
   license "MIT"
 
   def install
