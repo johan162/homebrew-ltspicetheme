ec0ba968541c0da70e06e2a08b4e4af2629552c6
Johan Persson <johan162@gmail.com>
2022-12-28 21:14:49 +0100

    Corrected sha256 for v2.0.3

--- themeltspice.rb
+++ themeltspice.rb
@@ -2,7 +2,7 @@ class Themeltspice < Formula
   desc "Set or create a named color theme for LTSpice Circuit Simulator"
   homepage "https://github.com/johan162/themeltspice"
   url "https://github.com/johan162/themeltspice/archive/refs/tags/v2.0.3.tar.gz"
-  sha256 "85c03a14a28e7c9e4c39faf5e95f9610bacdac4b6254c6eabf748ab82bf45982"
+  sha256 "21a9f3a5383b04b8eecb74270e58f528c6f1fe7b2fab96120888bddf0c25c5e1"
   license "MIT"
 
   def install
